f375c9f1f6b4efc2d4cead07036bf19021f672e1
Arulkharisma <arulkharisma49@gmail.com>
2024-07-15 18:00:26 +0700

    setup database

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $newsAll = News::all();
+        $newsNew = News::orderBy('created_at', 'desc')->take(10)->get();
+        return inertia::render('Hompage', [
+            'title' => 'Arkha News',
+            'description' => 'selamat datang di Arkha News',
+            'news' => $newsAll,
+            'newsnew' => $newsNew, 
+        ]);
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(News $news)
+    {
+       //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(News $news)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, News $news)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(News $news)
+    {
+        //
+    }
+}
