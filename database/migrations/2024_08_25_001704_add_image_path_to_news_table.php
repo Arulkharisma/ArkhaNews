80e9cd4dbbcc86f7c5d21de95afeef0aef681137
Arulkharisma <arulkharisma49@gmail.com>
2024-08-25 16:31:09 +0700

    add column image to db news

---
+++ database/migrations/2024_08_25_001704_add_image_path_to_news_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->string('image_path')->nullable();  // Menambahkan kolom image_path yang bisa bernilai null
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropColumn('image_path');  // Menghapus kolom image_path jika di-rollback
+        });
+    }
+};
