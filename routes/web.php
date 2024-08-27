027ee42d6c2674427939d337a88650a85f3e0904
Arulkharisma <arulkharisma49@gmail.com>
2024-08-27 14:35:38 +0700

    add routes for update and delete news

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,35 @@ Route::get('/welcome', function () {
 });
 
 
-Route::get('/', [NewsController::class, 'index']);
+Route::put('/news/{id}', [NewsController::class, 'update']);
+Route::post('/news', [NewsController::class, 'store']);
 Route::get('/news/{id}', [NewsController::class, 'show'])->name('news.show');
 Route::get('/category/{category}', [NewsController::class, 'showByCategory'])->name('news.byCategory');
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::post('/news/update/{id}', [NewsController::class, 'update'])
+    ->middleware(['auth', 'verified'])
+    ->name('update.news');
+
+
+Route::get('/', [NewsController::class, 'index']);
+// ->middleware(['auth', 'verified'])
+// ->name('hompage');
+
+
+Route::get('/dashboard', [NewsController::class, 'dashboard'])
+->middleware(['auth', 'verified'])
+->name('dashboard');
+
+Route::get('/news/edit/{id}', [NewsController::class, 'edit'])
+    ->middleware(['auth', 'verified'])
+    ->name('edit.news');
+
+Route::delete('/news/{id}', [NewsController::class, 'destroy'])
+    ->middleware(['auth', 'verified'])
+    ->name('delete.news');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
